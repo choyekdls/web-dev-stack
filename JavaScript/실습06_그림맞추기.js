e5207ac5c5e7a07cd58819acbb1d3de49f444540
choyekdls <popowoo32@gmail.com>
2025-05-28 17:56:49 +0900

    Create 실습06_그림맞추기.js

---
+++ JavaScript/실습06_그림맞추기.js
@@ -0,0 +1,8 @@
+// 그림 맞추기...랜덤.......
+const image = document.querySelectorAll("img");
+for (let i = 0; i < img.length; i++) {
+  console.log(i);
+}
+
+//setAttribute 사용해야함
+// 클릭 버튼 누르면 그림 맞추기 그림 배열 돌아가기......
