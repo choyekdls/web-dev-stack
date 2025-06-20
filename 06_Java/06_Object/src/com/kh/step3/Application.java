e208ff89453e697bce30f0e78b2c622f81d452ed
choyekdls <popowoo32@gmail.com>
2025-06-20 17:49:31 +0900

    Update Application.java

--- 06_Java/06_Object/src/com/kh/step3/Application.java
+++ 06_Java/06_Object/src/com/kh/step3/Application.java
@@ -28,6 +28,8 @@ public class Application {
 		System.out.println("빼기 : " + c.minus());
 		System.out.println("곱하기 : " + c.multiply(20,35));
 		System.out.println("나누기 : " + Calculator.divide(45,4));
+		System.out.println(c.factorial(5));
+		System.out.println(c.factorial2(5));
 	}
 
 }
