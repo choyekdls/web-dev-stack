374bf671dda899fd5ba5f5b56c32cecc4a6630a2
choyekdls <popowoo32@gmail.com>
2025-06-23 17:51:43 +0900

    Update SnackController.java

--- 06_Java/06_Object/src/com/kh/practice2/controller/SnackController.java
+++ 06_Java/06_Object/src/com/kh/practice2/controller/SnackController.java
@@ -6,13 +6,17 @@ public class SnackController {
 
 	private Snack s = new Snack();
 	
-	public String saveData(String kind, String name, String flavor, int numOf, int price) 
-	{   s.setKind(kind); s.setName(name);s.setFlavor(flavor);s.setNumOf(numOf);s.setPrice(price);
+	public String saveData(Snack snackData) {   
+		s.setKind(snackData.getKind()); 
+		s.setName(snackData.getName());
+		s.setFlavor(snackData.getFlavor());
+		s.setNumOf(snackData.getNumOf());
+		s.setPrice(snackData.getPrice());
 		return "데이터를 저장완료하였습니다.";
 	}
 	
 
-    public String confirmData() {
-		return s.getKind()+ "(" + s.getName() + " - " + s.getFlavor() + ")" + s.getNumOf() + "개 " + s.getPrice() + "원";
+    public Snack confirmData() {
+		return s;
 	}
 }
