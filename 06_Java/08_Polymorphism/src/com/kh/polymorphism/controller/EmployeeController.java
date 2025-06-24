6fa5203aa6575e6e64ccfbf5733073fb0405a04d
choyekdls <popowoo32@gmail.com>
2025-06-24 13:29:46 +0900

    Create EmployeeController.java

---
+++ 06_Java/08_Polymorphism/src/com/kh/polymorphism/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package com.kh.polymorphism.controller;
+
+import com.kh.polymorphism.model.Employee;
+import com.kh.polymorphism.model.Engineer;
+
+public class EmployeeController {
+
+	public Employee findEmployee(Employee[] emp) {
+		Employee findEmployee = null;
+		for (Employee employee : emp) {
+			if (employee.getName().equals("최다인")) {
+				findEmployee = employee;
+				
+			}
+		}
+		return findEmployee;
+	}
+	
+	public int getAnnualSalary(Employee findEmployee) {
+		if (findEmployee instanceof Engineer) {
+			Engineer engineer = (Engineer) findEmployee;
+			return engineer.getSalary() * 12 + engineer.getBonus();
+		}
+		return findEmployee.getSalary() * 12;
+		}
+
+	
+	public int totalSalary(Employee[] emp) {
+		int sum = 0;
+		for (Employee employee : emp) {
+			 sum += employee.getSalary();
+		}
+		return sum;
+	}
+}
