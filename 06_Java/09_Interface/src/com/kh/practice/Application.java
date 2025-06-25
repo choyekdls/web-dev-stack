acafa237b6c9d66bd34f395b1ef89991136e3936
choyekdls <popowoo32@gmail.com>
2025-06-25 17:23:35 +0900

    Update Application.java

--- 06_Java/09_Interface/src/com/kh/practice/Application.java
+++ 06_Java/09_Interface/src/com/kh/practice/Application.java
@@ -5,43 +5,41 @@ import java.util.Scanner;
 
 import com.kh.practice.controller.UserController;
 import com.kh.practice.model.Department;
+import com.kh.practice.model.DeptType;
 import com.kh.practice.model.UserInfo;
 
 public class Application {
-	
+
 	Scanner sc = new Scanner(System.in);
 	UserController uc = new UserController();
-	UserInfo userInfo = new UserInfo();
-	Department department = new Department();
+	UserInfo user = new UserInfo();
 
 	public static void main(String[] args) {
-		
+
 		Application app = new Application();
 		boolean check = true;
-		while(check) {
+		while (check) {
 			int select = app.employeeMenu();
-			switch(select) {
-				case 1:
-					app.insertEmp();
-					break;
-				case 2:
-					app.updateEmp();
-					break;
-				case 3:
-					app.printEmp();
-					break;
-				case 9:
-					System.out.println("프로그램을 종료합니다.");
-					check = false;
-					break;
-				default:
-					System.out.println("잘못 입력하셨습니다. 다시 입력해주세요~");
+			switch (select) {
+			case 1:
+				app.insertEmp();
+				break;
+			case 2:
+				app.updateEmp();
+				break;
+			case 3:
+				app.printEmp();
+				break;
+			case 9:
+				System.out.println("프로그램을 종료합니다.");
+				check = false;
+				break;
+			default:
+				System.out.println("잘못 입력하셨습니다. 다시 입력해주세요~");
 			}
 		}
-		
-		
 	}
-	
+
 	// 메인 메뉴를 출력하는 메서드
 	public int employeeMenu() {
 		System.out.println("1. 직원 정보 추가");
@@ -51,13 +49,12 @@ public class Application {
 		System.out.print("메뉴 번호를 누르세요 : ");
 		return Integer.parseInt(sc.nextLine());
 	}
-		
+
 	// 저장할 데이터를 사용자에게 받는 메서드
 	public void insertEmp() {
-		UserInfo u = new UserInfo();
 		System.out.println("직원 번호 : ");
-		u.setUserNo(Integer.parseInt(sc.nextLine()));
-		
+		int userNo = Integer.parseInt(sc.nextLine());
+
 		System.out.println("직원 아이디 : ");
 		String id = sc.nextLine();
 		System.out.println("직원 비밀번호 : ");
@@ -66,40 +63,51 @@ public class Application {
 		String email = sc.nextLine();
 		System.out.println("직원 이름 : ");
 		String name = sc.nextLine();
-		
 		System.out.println("추가 정보를 더 입력하시겠습니까?(y/n) : ");
-		String addInfo = sc.nextLine();
-		if(addInfo.equals("y")) {
-		// y일 경우만
-		System.out.println("직원 전화번호 : ");
-		String phone = sc.nextLine();
-		System.out.println("직원 주소 : ");
-		String addr = sc.nextLine();
-		System.out.println("직원 성별 : ");
-		String gender = sc.nextLine();
-		System.out.println("직원 생일 (예 : 2025-06-23) : "); // LocalDate.parse(문자열) <- 문자열을 날짜로
-		String bithDateInput = sc.nextLine();
-		LocalDate birthDate = LocalDate.parse(bithDateInput);
-		System.out.println("부서 번호 입력 : ");
-		System.out.println("부서명 입력 : "); // 너무 어렵다면 생략!	
-		String deptName = sc.nextLine();
-		department.setDeptName(deptName);
-		userInfo.setDepartment(department);
-		
-		} 
+		char addInfo = sc.nextLine().charAt(0);
+		if (addInfo == 'y') {
+			// y일 경우만
+			System.out.println("직원 전화번호 : ");
+			String phone = sc.nextLine();
+			System.out.println("직원 주소 : ");
+			String addr = sc.nextLine();
+			System.out.println("직원 성별 : ");
+			String gender = sc.nextLine();
+			System.out.println("직원 생일 (예 : 2025-06-23) : "); // LocalDate.parse(문자열) <- 문자열을 날짜로
+			String bithDateInput = sc.nextLine();
+			LocalDate birthDate = LocalDate.parse(bithDateInput);
+			// LocalDate birthDate = LocalDate.parse(sc.nextLine()); 도 가능
+//			System.out.println("부서명 : "); // 너무 어렵다면 생략!
+//			String deptName = sc.nextLine();
+			System.out.println("부서 코드: ");
+			int deptNo = Integer.parseInt(sc.nextLine());
+//			Department department = new Department();
+//			department.setDeptName(deptName);
+			// 추가적으로 들어가는 정보
+			//uc.addUser(phone, addr, gender, birthDate, department);
+			uc.addUser(phone, addr, gender, birthDate, deptNo);
+		}
+		// 기본적으로 들어가는 정보
+		uc.addUser(userNo, id, password, email, name);
 	}
-	
+
 	// 수정할 데이터를 사용자에게 받는 메서드
 	public void updateEmp() {
 		System.out.println("수정하려면 로그인이 필요합니다.");
 		System.out.print("아이디 입력 : ");
-		String id= sc.nextLine();
-		if 
+		String id = sc.nextLine();
 		System.out.print("비밀번호 입력 : ");
+		String password = sc.nextLine();
 		
+		// 로그인! 클라이언트(화면)단에도 유저 정보 임시 저장
+		user = uc.login(id, password);
+
 		// 아이디랑 비밀번호가 틀리다면!
-		System.out.println("로그인 실패! 정보 수정할 수 없습니다");
-		
+		if (user == null) {
+			System.out.println("로그인 실패! 정보 수정할 수 없습니다");
+			return;
+		}
+
 		// 아이디와 비밀번호 성공했을시에만!
 		System.out.println("직원의 어떤 정보를 수정하시겠습니까?");
 		System.out.println("1. 전화 번호");
@@ -108,29 +116,34 @@ public class Application {
 		System.out.println("9. 돌아가기");
 		System.out.print("메뉴 번호를 누르세요 : ");
 		int select = Integer.parseInt(sc.nextLine());
-		
-		switch(select) {
-			case 1:
-				System.out.print("전화 번호 입력 : ");
-				break;
-			case 2:
-				System.out.print("생일 입력 (예 : 2025-06-23) : ");
-				break;
-			case 3:
-				System.out.print("부서 입력 : ");
-				break;
-			case 9:
-				break;
+
+		switch (select) {
+		case 1:
+			System.out.print("전화 번호 입력 : ");
+			String phone = sc.nextLine();
+			user.setPhone(phone);
+			break;
+		case 2:
+			System.out.print("생일 입력 (예 : 2025-06-23) : ");
+			LocalDate birthDate = LocalDate.parse(sc.nextLine());
+			user.setBirthDate(birthDate);
+			break;
+		case 3:
+			System.out.print("부서 입력 : ");
+			String deptName = sc.nextLine();
+			Department department = new Department();
+			department.setDeptName(deptName);
+			user.setDepartment(department);
+			break;
+		case 9:
+			break;
 		}
+
+		uc.updateUser(user);
 	}
-	
+
 	// 데이터를 출력하는 메서드
 	public void printEmp() {
-			for(UserInfo userInfo : user) {
-				if(user!=null) {
-					System.out.println(user);
-				}
-			}
-		}
+		System.out.println(user);
 	}
-		
+}
