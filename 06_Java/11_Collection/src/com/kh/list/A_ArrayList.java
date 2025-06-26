3f2c2ba56d9b7919b577ae8f2c7bff46d4368d22
choyekdls <popowoo32@gmail.com>
2025-06-26 17:26:14 +0900

    Update A_ArrayList.java

--- 06_Java/11_Collection/src/com/kh/list/A_ArrayList.java
+++ 06_Java/11_Collection/src/com/kh/list/A_ArrayList.java
@@ -1,6 +1,8 @@
 package com.kh.list;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import com.kh.list.model.Person;
 
@@ -32,47 +34,92 @@ import com.kh.list.model.Person;
  * */
 
 public class A_ArrayList {
-	
+
 	/*
-	 * 제네릭(Generics)
-	 * - 컴파일시 타입을 체크해 주는 기능
-	 * - <> 다이아몬드 연산자 사용
+	 * 제네릭(Generics) - 컴파일시 타입을 체크해 주는 기능 - <> 다이아몬드 연산자 사용
 	 * 
-	 * * 컬렉션에서 사용하는 이류
-	 * -> 명시된 타입의 객체만 저장하도록 제한을 두기 위해
-	 * */
-	
+	 * * 컬렉션에서 사용하는 이류 -> 명시된 타입의 객체만 저장하도록 제한을 두기 위해
+	 */
+
 	public void method1() {
 		ArrayList<String> list = new ArrayList<String>();
-		//list.add(1);
+		// list.add(1);
 		list.add("이승민");
 		list.add(null);
-		//list.add(true);
+		// list.add(true);
 		System.out.println(list);
 	}
-	
+
 	public void method2() {
 		ArrayList<Person> list = new ArrayList<>();
-		
+
 		// 1. add : 추가
 		list.add(new Person("전현무", "삼성동", 47));
 		list.add(new Person("박나래", "이태원동", 39));
 		// 인덱스를 지정하여 해당 인덱스에 추가
 		// -> 내부적으로 기존에 있는 것들은 뒤로 밀리고 해당 인덱스에 추가
 		list.add(1, new Person("기안84", "과천시", 40));
-		
+
 		// 2. set : 해당 인덱스의 값 변경 (수정)
 		list.set(0, new Person("키", "한남동", 33));
-		
+
 		// 3. size : 리스트 안에 몇 개의 데이터가 있는지
 		System.out.println("사람 수 : " + list.size());
-		
+
 		// 4. remove : 해당 인덱스의 객체 삭제
 		list.remove(1);
-		
+
 		// 5. get : 해당 인덱스의 객체 가져오기
-        System.out.println(list.get(0));
+		System.out.println(list.get(0));
+
+		// 6. subList : 기존 리스트에서 원하는 만큼 추출해서 새로운 리스트 반환
+		list.add(new Person("전현무", "삼성동", 47));
+		list.add(new Person("기안84", "과천시", 40));
+
+		List<Person> subList = list.subList(0, 2);
+		System.out.println(subList);
+
+		// 7. addAll : 컬렉션을 통째로 추가
+		list.addAll(subList);
+
+		// 8. isEmpty : 컬렉션이 비어있는지
+		System.out.println("리스트가 비어있는지?" + list.isEmpty());
+
+		// 리스트에 저장된 사람들의 평균 연령 출력
+		int sum = 0;
+		for (int i = 0; i < list.size(); i++) {
+			Person person = list.get(i);
+			sum += person.getAge();
+			System.out.println(sum / list.size());
+		}
+
+		// 저장된 사람들의 이름만 출력
+		for (Person person : list) {
+			System.out.println(person.getName());
+		}
+		// 나이 순서대로 정렬 -> 이름순으로
+		Collections.sort(list);
+		System.out.println(list);
 		
+		// 9. clear : 싹 비우기
+		list.clear();
+
+		System.out.println(list);
+	}
+
+	public void method3() {
+		List<String> list = new ArrayList<>();
+		list.add("banana");
+		list.add("orange");
+		list.add("apple");
+		list.add("mango");
+		list.add("grape");
+
+		// 10. 오름차순 정렬 : Comparable 인터페이스를 구현하고 있는 요소를 가지고 비교값들 반환하여 정렬
+		Collections.sort(list);
+		// 11. 내림차순 정렬 : sort 메서드로 오름차순 정렬 후 reverse 로 정렬해야 정확함. 그냥 reverse해버리면 리스트에 입력한
+		// 그대로 내림차순 정렬함
+		Collections.reverse(list);
 		System.out.println(list);
 	}
 
@@ -80,7 +127,7 @@ public class A_ArrayList {
 		A_ArrayList a = new A_ArrayList();
 //		a.method1();
 		a.method2();
+//		a.method3();
 
 	}
-
 }
