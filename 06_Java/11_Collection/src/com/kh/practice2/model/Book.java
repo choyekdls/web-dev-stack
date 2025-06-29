bf370d7e2d2bcc10449e90a0befbc95e41280348
choyekdls <popowoo32@gmail.com>
2025-06-30 08:49:12 +0900

    Update Book.java

--- 06_Java/11_Collection/src/com/kh/practice2/model/Book.java
+++ 06_Java/11_Collection/src/com/kh/practice2/model/Book.java
@@ -12,5 +12,12 @@ public class Book {
 	private String title;
 	private boolean coupon;
 	private int accessAge;
+	private int count; // 각 책 당 대여된 수
+	
+	public Book(String title, boolean coupon, int acessAge) {
+		this.title = title;
+		this.coupon = coupon;
+		this.accessAge = accessAge;
+	}
 
 }
