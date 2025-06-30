c9d9dfb8c38b32b8868a585669dca80b6e2f0e34
choyekdls <popowoo32@gmail.com>
2025-06-30 14:56:29 +0900

    Update Music.java

--- 06_Java/11_Collection/src/com/kh/practice3/model/Music.java
+++ 06_Java/11_Collection/src/com/kh/practice3/model/Music.java
@@ -7,9 +7,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Music {
+public class Music implements Comparable<Music> {
 
 	private String artist;
 	private String song;
 
+	
+	@Override
+	public int compareTo(Music o) {
+		return this.artist.compareTo(o.artist);
+	}
 }
