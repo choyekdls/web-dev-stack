12c99f5c863cc435a8dfb817d0b75a5405a64c29
choyekdls <popowoo32@gmail.com>
2025-07-03 17:49:38 +0900

    Update PersonView.java

--- 07_JDBC/03_PersonMVC/src/view/PersonView.java
+++ 07_JDBC/03_PersonMVC/src/view/PersonView.java
@@ -1,50 +1,79 @@
 package view;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
 
-import controller.PersonController;
+import controller.PersonController2;
+import vo.Person;
 
 public class PersonView {
 
+	// 테스트 용도!
+	PersonController2 pc = new PersonController2();
 	Scanner sc = new Scanner(System.in);
-	PersonController pc = new PersonController();
 
-		// 테스트 용도!
+	public static void main(String[] args) {
+		PersonView view = new PersonView();
+
+		while (true) {
 		
-	public void mainMenu() throws SQLException {
-		boolean check = true;
-		while (check) {
+			System.out.println(view.addPerson());
+
+		}
+	}
+	
+	public void menu() {
+		while (true) {
 			System.out.println("1. 신규 사용자 등록");
 			System.out.println("2. 전체 사용자 조회");
 			System.out.println("3. 사용자 검색 및 조회");
 			System.out.println("4. 사용자 정보 수정");
 			System.out.println("5. 사용자 정보 삭제");
 			System.out.print("메뉴 번호 입력 : ");
-			int num = Integer.parseInt(sc.nextLine());
-
-			switch (num) {
+			int select = Integer.parseInt(sc.nextLine());
+			
+			switch(select) {
 			case 1:
-				pc.addPerson();
+				System.out.println(addPerson());
 				break;
 			case 2:
-				pc.searchAllPerson();
-				break;
-			case 3:
-				pc.searchPerson();
-				break;
-			case 4:
-				pc.updatePerson();
-				break;
-			case 5:
-				pc.removePerson();
-				break;
-			default:
-				System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
+				System.out.println(searchAllPerson());
+				
 			}
-}
-		
+			}
 	}
-	
+
+	public String addPerson() {
+
+		try {
+			System.out.println("이름 > ");
+			String name = sc.nextLine();
+			System.out.println("나이 > ");
+			int age = Integer.parseInt(sc.nextLine());
+			System.out.println("주소 > ");
+			String addr = sc.nextLine();
+			return pc.addPerson(name, age, addr);
+			// 1인 경우가 추가!
+
+		} catch (NumberFormatException e) {
+			return "숫자를 입력해주세요";
+
+		}
+
+		public void searchAllPerson() {
+			try {
+			List<Person> list = pc.searchAllPerson();
+			for(Person p : list) {
+				System.out.println(p);
+			}
+			} catch (SQLException e) {
+			e.printStackTrace();
+			}
+		pc.searchPerson();
+		pc.updatePerson();
+		pc.removePerson();
+
+	}
 
 }
