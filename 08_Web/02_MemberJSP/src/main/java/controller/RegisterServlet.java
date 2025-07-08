b9bb583a09622d87cdaafdd22f9457e51806e093
choyekdls <popowoo32@gmail.com>
2025-07-08 17:53:36 +0900

    Update RegisterServlet.java

--- 08_Web/02_MemberJSP/src/main/java/controller/RegisterServlet.java
+++ 08_Web/02_MemberJSP/src/main/java/controller/RegisterServlet.java
@@ -27,18 +27,40 @@ public class RegisterServlet extends HttpServlet {
 		String pwd = request.getParameter("pwd");
 		int age = Integer.parseInt(request.getParameter("age"));
 
+		boolean check = true;
+
 		// 2. DAO 호출 - DB 접근 필요시
-	    MemberDAO dao = new MemberDAO();
-	    
-	    try {
+		MemberDAO dao = new MemberDAO();
+
+		try {
 			dao.register(new Member(id, name, pwd, age));
 		} catch (SQLException e) {
-			e.printStackTrace();
+			check = false;
 		}
-	    
-	    // 3. 결과 페이지
-	    //  짜잔!회원가입이 완료되었습니다~!
-	    response.sendRedirect("result.jsp");
+
+		// 3. 바인딩 :  결과 페이지에 서버에서 받은 값 보내야 할 때
+		// 바인딩은 여러개해도 상관없다고 함
+		//request.setAttribute("name", name);
+		request.setAttribute("check", check);
+		
+		// 4. 네비게이션 : 결과 페이지 지정
+		// 회원가입 성공할 때랑 실패할 때 페이지를 구분하는 것
+		// check가 true인 경우는 회원가입 성공 페이지로 이동
+		//if (check) {
+			//response.sendRedirect("result.jsp"); 
+			// 만약 결과 페이지로 서버에서 받은 값 보여주려면 response는 사용이 불가함
+			// RequestDispatcher forward  방식으로 보내야함
+		
+		    // 결과 페이지는 하나로
+			request.getRequestDispatcher("result.jsp").forward(request, response);
+			// 또 다시 request할 일이 있다는 뜻
+		
+			//} else {
+			//response.sendRedirect("fail.jsp");
+			// 응답하고 끝
+		//}		
+		// false인 경우는 실패 페이지로 이동 -> fail.jsp
+
 	}
 
 }
