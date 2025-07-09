aab03ac43095a7fb74f3658f5d0f6bfe2f34f28f
choyekdls <popowoo32@gmail.com>
2025-07-09 17:51:35 +0900

    Update LogoutServlet.java

--- 08_Web/04_MemberLogin/src/main/java/cotroller/LogoutServlet.java
+++ 08_Web/04_MemberLogin/src/main/java/cotroller/LogoutServlet.java
@@ -5,37 +5,29 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import vo.Member;
+
 import java.io.IOException;
 
-/**
- * Servlet implementation class LogoutServlet
- */
-@WebServlet("/LogoutServlet")
+@WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LogoutServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+ 
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		HttpSession session = request.getSession();
+		
+		Member member = (Member) session.getAttribute("member");
+		if(member != null) {
+			session.invalidate();
+		}
+		
+		response.sendRedirect("/");
+		
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
