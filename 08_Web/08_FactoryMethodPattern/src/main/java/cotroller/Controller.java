87577902ec4d693b6625133e0b94b0fb469172cc
choyekdls <popowoo32@gmail.com>
2025-07-10 10:58:49 +0900

    Create Controller.java

---
+++ 08_Web/08_FactoryMethodPattern/src/main/java/cotroller/Controller.java
@@ -0,0 +1,9 @@
+package cotroller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public interface Controller {
+
+	public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception;
+}
