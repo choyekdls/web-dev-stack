070da32c3ed41a3d36b5615b8815ed4b9f83e13c
choyekdls <popowoo32@gmail.com>
2025-07-14 17:41:31 +0900

    Create MemberMapper.java

---
+++ 09_Spring/01_MyBatis/src/main/java/com/kh/mybatis/mapper/MemberMapper.java
@@ -0,0 +1,18 @@
+package com.kh.mybatis.mapper;
+/*
+ * 기존의 DAO 역할을 합니다
+ * */
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.kh.mybatis.model.vo.Member;
+
+@Mapper
+public interface MemberMapper {
+    void register(Member vo); // 받은 값을 DB에
+    Member login(Member vo);
+    List<Member> allMember();
+    void update(Member vo);
+}
