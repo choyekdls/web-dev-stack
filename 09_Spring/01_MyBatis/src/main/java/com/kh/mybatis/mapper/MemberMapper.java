3744568367cf85e94b62ba923335e29c0481b30c
choyekdls <popowoo32@gmail.com>
2025-07-16 09:01:08 +0900

    Update MemberMapper.java

--- 09_Spring/01_MyBatis/src/main/java/com/kh/mybatis/mapper/MemberMapper.java
+++ 09_Spring/01_MyBatis/src/main/java/com/kh/mybatis/mapper/MemberMapper.java
@@ -7,12 +7,15 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.kh.mybatis.model.dto.SearchDTO;
 import com.kh.mybatis.model.vo.Member;
 
 @Mapper
 public interface MemberMapper {
     void register(Member vo); // 받은 값을 DB에
     Member login(Member vo);
+    List<Member> search(SearchDTO dto);
     List<Member> allMember();
     void update(Member vo);
+    void delete(String id);
 }
