89d1cca9cc3cee2f7bda592d375628c86cc9f12b
choyekdls <popowoo32@gmail.com>
2025-07-17 12:22:34 +0900

    Create MemberService.java

---
+++ 09_Spring/02_Ajax/src/main/java/com/kh/ajax/service/MemberService.java
@@ -0,0 +1,21 @@
+package com.kh.ajax.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.ajax.mapper.MemberMapper;
+import com.kh.ajax.vo.Member;
+
+@Service
+public class MemberService implements MemberMapper {
+	
+	@Autowired
+	private MemberMapper mapper;
+
+	@Override
+	public Member idCheck(String id) {
+		
+		return null;
+	}
+
+}
