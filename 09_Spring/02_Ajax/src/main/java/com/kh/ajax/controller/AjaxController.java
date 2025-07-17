fc11afa9822a2dcf809aa84853c620163f0af6c9
choyekdls <popowoo32@gmail.com>
2025-07-17 09:14:58 +0900

    Create AjaxController.java

---
+++ 09_Spring/02_Ajax/src/main/java/com/kh/ajax/controller/AjaxController.java
@@ -0,0 +1,31 @@
+package com.kh.ajax.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AjaxController {
+
+    private final PageController pageController;
+	
+	private int count = 0;
+
+    AjaxController(PageController pageController) {
+        this.pageController = pageController;
+    }
+
+	@ResponseBody
+	@GetMapping("/count")
+	public int count() {
+		System.out.println("ajax 요청!");
+		return ++count;
+	}
+	
+	@ResponseBody
+	@GetMapping("/encoding")
+	public String encoding (String nickname) {
+		System.out.println(nickname);
+		return nickname;
+	}
+}
