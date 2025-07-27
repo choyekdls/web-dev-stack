0dd32bc44aa740c6880e5287b5fcb112631f3c66
choyekdls <popowoo32@gmail.com>
2025-07-28 08:38:57 +0900

    250725_오전 수업_FILE UPLOAD

--- 09_Spring/04_FileUpload/src/main/java/com/kh/upload/controller/BoardController.java
+++ 09_Spring/04_FileUpload/src/main/java/com/kh/upload/controller/BoardController.java
@@ -5,13 +5,24 @@ import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.kh.upload.Service.BoardService;
+import com.kh.upload.dto.BoardDTO;
+import com.kh.upload.vo.Board;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class BoardController {
+	
+	@Autowired
+	private BoardService service;
 
 	private final CustomerController customerController;
 
@@ -38,7 +49,7 @@ public class BoardController {
 		} catch (IllegalStateException | IOException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return fileName;
 	}
 
 	@PostMapping("/upload")
@@ -47,17 +58,8 @@ public class BoardController {
 		System.out.println("파일 사이즈 : " + file.getSize());
 		System.out.println("파일 파라미터명 : " + file.getName());
 
-		// 중복 방지를 위한 UUID 적용
-		UUID uuid = UUID.randomUUID();
-		String fileName = uuid.toString() + "_" + file.getOriginalFilename();
-
-		File copyFile = new File("\\\\192.168.0.35\\upload\\" + fileName);
-
-		try {
-			file.transferTo(copyFile);
-		} catch (IllegalStateException | IOException e) {
-			e.printStackTrace();
-		}
+		String fileName = fileUpload(file);
+		//http://localhost:8081/ + fileName <- url
 
 		return "redirect:/";
 	}
@@ -68,19 +70,30 @@ public class BoardController {
 
 		// 마..쉽지 않네..
 		for (MultipartFile file : files) {
-			UUID uuid = UUID.randomUUID();
-			String fileName = uuid.toString() + "_" + file.getOriginalFilename();
-
-			File copyFile = new File("\\\\192.168.0.35\\upload\\" + fileName);
-
-			try {
-				file.transferTo(copyFile);
-			} catch (IllegalStateException | IOException e) {
-				e.printStackTrace();
-			}
+			String fileName = fileUpload(file);
 		}
 
 		return "redirect:/";
+	}
+	
+	@GetMapping("/list")
+	public String list() {
+		return "/list";
+	}
+	
+	@PostMapping("/write")
+	public String uploadBoard(BoardDTO dto) {
+		String url = fileUpload(dto.getFile());
+		
+		Board board = new Board();
+		board.setTitle(dto.getTitle());
+		board.setContent(dto.getContent());
+		board.setUrl(url);
+		System.out.println(board.getTitle());
+		System.out.println(board.getContent());
+		System.out.println(board.getUrl());
+		service.uploadBoard(board);
+	    return "/list";
 	}
 
 }
